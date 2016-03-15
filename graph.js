2d0fd95b5c55b3c767de35415c896307e1df18e9
Daniel Good <danrgood@gmail.com>
2016-03-15 13:29:53 -0400

    march 15.1

--- graph.js
+++ graph.js
@@ -16,24 +16,19 @@ function graph() {
         nodeEdges.forEach(function iter(element) {
       
     
-        
-        /////
+
             for (var inner in nodes){
             if(inner == element){
                 nodes[inner].edges.push(id);
             }
         }
         
-        ////
-
 
     });
 
   }
   this.addEdge = function(id1, id2) {
-    //add edge to edge array
-    //search through node array, find nodes
-    //add edges to the node's edge arrays
+  
     
     
       nodes[id1].edges.push(id2);
@@ -46,45 +41,23 @@ function graph() {
      //1)remove node from node array
      //2)remove node from node's edge array
       
-      
-      //1)
-      /*
-      var len = nodes.length;
-      for(var i=0; i<len; i++){
-          if(nodes[i].id == id){
-              nodes = nodes.slice(0, i).concat(nodes.slice(i+1, len));
-              len--;
-              i--;
-          }else{
-              var elen = nodes[i].edges.length;
-              for(var j=0; j<elen; j++){
-                  if(nodes[i].edges[j][0] == id || nodes[i].edges[j][0]==id){
-                      //3)
-                      nodes[i].edges = nodes[i].edges.slice(0, j).concat(nodes[i].edges.slice(i+1, elen));
-                      elen--;
-                      j--;
-                  }
-              }
-              
-          }
-      }
-      */
-      //1)
-      delete nodes.id;
+
+     
       //2)
       //iter through nodes
       for(var node in nodes){
           //iter thourgh it's edges
-          var len = node.edges.length;
+          var len = nodes[node].edges.length;
           for(var i=0; i<len; i++){
-                  if(node.edges[i] == id){
+                  if(nodes[node].edges[i] == id){
                       
-                      node.edges = node.edges.slice(0, i).concat(node.edges.slice(i+1, len));
+                      nodes[node].edges = nodes[node].edges.slice(0, i).concat(nodes[node].edges.slice(i+1, len));
                       len--;
                       i--;
                   }
               }
       }
+    delete nodes[id];
   }
   this.removeEdge = function(id1, id2) {
      //remove all reference to edge
@@ -105,12 +78,7 @@ function graph() {
    });
 
   }
-  this.print = function() {
-    console.log("Nodes");
-    console.log("");
-   
-  
-  }
+ 
   this.getNodes= function(){
       return nodes;
   }
--- tests.js
+++ tests.js
@@ -1,10 +1,4 @@
 
-/*	
-QUnit.test( "hello test", function( assert ) {
-	assert.ok( 1 == "1", "Passed!" );
-	});
-*/
-
 
 
 QUnit.test("Graph addNode", function( assert ) {
@@ -44,9 +38,22 @@ QUnit.test("Graph addEdges", function( assert ) {
     
     
 });
-/*
+
 QUnit.test("Graph removeNodes", function( assert ) {
+    var myGraph = new graph();
+    myGraph.addNode("k", "kitchen", []);
+    myGraph.addNode("bath", "bathroom", []);
+    myGraph.addNode("living room", "living room", []);
+    myGraph.addNode("bed1", "bedroom 1", []);
+    myGraph.addNode("bed2", "bedroom 2", []);
+    myGraph.addEdge("bed1", "bed2");
+    myGraph.addEdge("k", "bath");
+    myGraph.addEdge("bath", "bed1");
+    myGraph.removeNode("k");
+    assert.deepEqual(myGraph.getNodes(), {"bath":{node:"bathroom", edges:["bed1"]}, "living room":{node: "living room", edges:[]}, "bed1":{node: "bedroom 1", edges:["bed2", "bath"]}, "bed2": {node:"bedroom 2", edges:["bed1"]}});
 });
+
+
 QUnit.test("Graph removeEdges", function( assert ) {
     
 });
@@ -54,11 +61,7 @@ QUnit.test("duplicate ID", function(assert){
     
     
 });
-QUnit.test("edges equal test", function(assert){
-    //on second thought maybe I should get rid of edges object
-    //because duplication of data is bad
-    
-});
+
            
 
-*/
+
